c8a5d96f5fb291c87f0a7383791d994f66b7444d
ZQWhiting <zach.whiting@icloud.com>
2020-05-16 14:38:14 -0600

    added random character selector

--- Develop/script.js
+++ Develop/script.js
@@ -20,8 +20,26 @@
 
 // Assignment code here
 
+
+// Password Character Arrays
+passCritArray = [
+  0, 1, 2, 3,
+];
+lowercaseArray = [
+  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+];
+uppercaseArray = [
+  "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+];
+numericArray = [
+  "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+];
+specialCharArray = [
+  " ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~",
+];
 // Password Criteria Functions
 var passCritFunctions = function () {
+  // Length
   var length = function () {
     var length = prompt("Select password length. (Between 8 and 128)");
     if (length >= 8 && length <= 128) {
@@ -32,6 +50,7 @@ var passCritFunctions = function () {
       this.length();
     }
   };
+  // Lowercase
   var lowercase = function () {
     var lowercase = prompt("Include lowercase? (y/n)");
     lowercase = lowercase.toLowerCase();
@@ -46,6 +65,7 @@ var passCritFunctions = function () {
       this.lowercase();
     }
   };
+  // Uppercase
   var uppercase = function () {
     var uppercase = prompt("Include uppercase? (y/n)");
     uppercase = uppercase.toLowerCase();
@@ -60,6 +80,7 @@ var passCritFunctions = function () {
       this.uppercase();
     }
   };
+  // Numeric
   var numeric = function () {
     var numeric = prompt("Include numeric values? (y/n)");
     numeric = numeric.toLowerCase();
@@ -74,6 +95,7 @@ var passCritFunctions = function () {
       this.numeric();
     }
   };
+  // Special Characters
   var specialChar = function () {
     var specialChar = prompt("Include special characters? (y/n)");
     specialChar = specialChar.toLowerCase();
@@ -97,38 +119,46 @@ var passCritFunctions = function () {
   }
   return passwordCrit;
 };
-passCritArray = [
-  {
-    lowercaseArray =[
-      "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-    ]
-  },
-  {
-    uppercaseArray =[
-      "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-    ]
-  },
-  {
-    numericArray =[
-      "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-    ]
-  },
-  {
-    specialCharArray =[
-      " ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~",
-    ]
-  },
-];
+
+// Character Selector
+var charSelect = function () {
+  var charSelector = function () {
+    var randomArray = function () {
+      var arraySelect = passCritArray[Math.floor(Math.random() * passCritArray.length)];
+      return arraySelect;
+    };
+    var array = randomArray();
+    switch (array) {
+      case 0:
+        var char = lowercaseArray[Math.floor(Math.random() * lowercaseArray.length)];
+        return char;
+      case 1:
+        var char = uppercaseArray[Math.floor(Math.random() * uppercaseArray.length)];
+        return char;
+      case 2:
+        var char = numericArray[Math.floor(Math.random() * numericArray.length)];
+        return char;
+      case 3:
+        var char = specialCharArray[Math.floor(Math.random() * specialCharArray.length)];
+        return char;
+      default:
+        break;
+    }
+  };
+  var character = charSelector();
+  return character;
+};
 
 // Generate Password Function
 var generatePassword = function () {
   passCritFunctions(); //passwordCrit.length.lowercase.uppercase.numeric.specialChar
+  for (i = 0; i < passwordCrit.length; i++) {
+    var character = charSelect(); // Random Character
 
-
-  return
+  }
 };
-
-
+//
+//
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
