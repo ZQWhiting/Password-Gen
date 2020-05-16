4d76662e3611eb15821af2fcdde5eb9d3718c9cd
ZQWhiting <zach.whiting@icloud.com>
2020-05-16 12:33:10 -0600

    added passCritArray

--- Develop/script.js
+++ Develop/script.js
@@ -97,13 +97,35 @@ var passCritFunctions = function () {
   }
   return passwordCrit;
 };
+passCritArray = [
+  {
+    lowercaseArray =[
+      "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+    ]
+  },
+  {
+    uppercaseArray =[
+      "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+    ]
+  },
+  {
+    numericArray =[
+      "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    ]
+  },
+  {
+    specialCharArray =[
+      " ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~",
+    ]
+  },
+];
 
 // Generate Password Function
 var generatePassword = function () {
-passCritFunctions();
+  passCritFunctions(); //passwordCrit.length.lowercase.uppercase.numeric.specialChar
 
 
- return
+  return
 };
 
 
